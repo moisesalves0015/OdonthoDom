1c819aeecf91b269cf79f8990b1a9eec8b624925
Moises Santos <moisesalves0015@gmail.com>
2024-12-15 22:52:51 -0300

    Add Banner informativo com botoes e contato

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -5,7 +5,20 @@ function Banner() {
   return (
     
     <div className='BannerConteiner'>
-     <div></div>
+     <div>
+      <h1>Clínica Odontológica completa para o seu sorriso!</h1>
+      <p>Proporcionamos um atendimento humanizado e de alta qualidade
+      saúde bucal, garantindo que nossos pacientes se sintam acolhidos
+       e confiantes ao cuidar de sua saúde.</p>
+       <section>
+      <button>Agendar uma avaliação </button>
+      <img src="/contato.svg" alt="Descrição da imagem" />
+      <section>
+        <h5>Contato</h5>
+        <h4>99 99999-9999</h4>
+      </section>
+      </section>
+     </div>
      <div></div>
     </div>
     
