5e7c9ea0306ab59dc5e35538a14e48d72602a8b6
Moises Santos <moisesalves0015@gmail.com>
2025-01-26 21:55:35 -0300

    Adicionado serviço ortondontia

--- src/FeedServicos/Serviços.jsx
+++ src/FeedServicos/Serviços.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import './Serviços.css';
 import ServiçoImplante from './ServiçoImplante';
+import ServiçoOrtondontia from './ServiçoOrtondontia';
 
 function Serviços() {
   return (
@@ -11,7 +12,7 @@ function Serviços() {
     <p>Na nossa clínica, você encontra especialidades odontológicas voltadas para oferecer o melhor cuidado à sua saúde bucal. Seja qual for a sua necessidade, nossa equipe de profissionais está preparada para atender com tecnologia, experiência e um atendimento humanizado. Confira nossas especialidades e escolha a melhor solução para o seu sorriso!</p>
     </div> 
       <ServiçoImplante />
-      <ServiçoImplante />
+      <ServiçoOrtondontia />
       <ServiçoImplante />
       <ServiçoImplante />
     </>
